5bb13607a1770ae5f3fb7d2b8402db2d1ad616eb
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-10-30 14:53:43 +0100

    Remove Projects from Portfolio

--- src/components/Portfolio/ProjectsData.js
+++ src/components/Portfolio/ProjectsData.js
@@ -1,12 +1,5 @@
-import DisneyClone from '../../assets/disney-clone.webp';
 import CrwnClothing from '../../assets/crwn-clothing.webp';
 import MusicApp from '../../assets/music-app.webp';
-import ArchStudio from '../../assets/arch-studio.webp';
-import Photosnap from '../../assets/photosnap.webp';
-import Natours from '../../assets/natours.webp';
-import Trillo from '../../assets/trillo.webp';
-import Nexter from '../../assets/nexter.webp';
-import BeatBox from '../../assets/beat-box.webp';
 
 const ProjectsData = [
   {
@@ -20,48 +13,6 @@ const ProjectsData = [
     name: 'Music App',
     github: 'https://github.com/nejmiyassine01/music-app',
     preview: 'https://music-black.vercel.app/',
-  },
-  {
-    image: DisneyClone,
-    name: 'Disney+ Clone',
-    github: 'https://github.com/nejmiyassine01/disneyplus',
-    preview: 'https://disney-clone-e8b0b.web.app/',
-  },
-  {
-    image: ArchStudio,
-    name: 'Arch Studio',
-    github: 'https://github.com/nejmiyassine01/arch',
-    preview: 'https://arch-git-main-nejmiyassine01.vercel.app/',
-  },
-  {
-    image: Photosnap,
-    name: 'PhotoSnap App',
-    github: 'https://github.com/nejmiyassine01/photosnap-app',
-    preview: 'https://photosnap-app.nejmiyassine01.vercel.app/',
-  },
-  {
-    image: Trillo,
-    name: 'Trillo',
-    github: 'https://github.com/nejmiyassine01/Trillo',
-    preview: 'https://trillo-lovat.vercel.app/',
-  },
-  {
-    image: Nexter,
-    name: 'Nexter',
-    github: 'https://github.com/nejmiyassine01/nexter',
-    preview: 'https://nexter-nu.vercel.app/',
-  },
-  {
-    image: Natours,
-    name: 'Natours',
-    github: 'https://github.com/nejmiyassine01/natours',
-    preview: 'https://natours-jade.vercel.app/',
-  },
-  {
-    image: BeatBox,
-    name: 'Beat Box',
-    github: 'https://github.com/nejmiyassine01/beat-app',
-    preview: 'https://beat-app.vercel.app/',
   },
 ];
 
